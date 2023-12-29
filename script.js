dcb587962aa80efa463ff2223bfd520b58a497c3
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2023-12-28 22:39:21 -0500

    Add images, social media footer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Where the Wild Things Are
 
 const h2 = document.createElement("h2");
-h2.textContent = "This text added by Javascript";
+h2.textContent = "Powered by Javascript";
 
 const main = document.getElementById("main");
 main.append(h2);
