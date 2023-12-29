ac32a68b7bd92cbbd907d13a531935de82137bcc
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2023-12-29 15:15:15 -0500

    Nav bar reveals content

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 // Where the Wild Things Are
+function poweredBy() {
+    const h2 = document.createElement("h2");
+    h2.textContent = "Powered by Javascript";
 
-const h2 = document.createElement("h2");
-h2.textContent = "Powered by Javascript";
-
-const content = document.getElementById("content");
-content.append(h2);
+    const content = document.getElementById("content");
+    content.append(h2);
+}
 
 const navItems = document.querySelectorAll("#nav li");
-navItems.forEach(currentItem => 
-    currentItem.addEventListener("click", function ()  {
-        navItems.forEach(item =>
-            item.classList.remove("selected")
-        );
-        currentItem.classList.toggle("selected");
-}));
+const contentItems = document.querySelectorAll("#content div")
 
+function clearPageContent() {
+    contentItems.forEach(item =>
+        item.classList.remove("selected")
+    )
+}
+
+function showPageContent(pageClass) {
+    const activePage = document.querySelector(`#content div.${pageClass}`);
+    activePage.classList.toggle('selected');
+}
+
+function initialPageState(show) {
+    poweredBy();
+    initializeNav();
+    showPageContent(`${show}`);
+}
+
+function initializeNav() {
+    navItems.forEach(currentItem => 
+        currentItem.addEventListener("click", function ()  {
+            navItems.forEach(item =>
+                item.classList.remove("selected")
+            );
+            clearPageContent();
+            document.querySelector(`div.${currentItem.classList.value}`).classList.toggle("selected");
+            currentItem.classList.toggle("selected");
+    }));
+}
+
+
+initialPageState('about');
