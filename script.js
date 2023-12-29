22951c3137cd0edf26c65bdcfcc4c0c3096f7d04
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2023-12-29 12:58:44 -0500

    Working toggle on selected nav items

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ h2.textContent = "Powered by Javascript";
 
 const content = document.getElementById("content");
 content.append(h2);
+
+const navItems = document.querySelectorAll("#nav li");
+navItems.forEach(currentItem => 
+    currentItem.addEventListener("click", function ()  {
+        navItems.forEach(item =>
+            item.classList.remove("selected")
+        );
+        currentItem.classList.toggle("selected");
+}));
+
