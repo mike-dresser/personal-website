e558753729ffbb7e28e979d2d9ff78822e4dc284
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2023-12-28 21:55:49 -0500

    Add script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Where the Wild Things Are
+
+const h2 = document.createElement("h2");
+h2.textContent = "This text added by Javascript";
+
+const main = document.getElementById("main");
+main.append(h2);
+
